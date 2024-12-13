0df11dd7073a9dd574f6673b39972f9a1c27ab5b
insertnull <jcarpon2021@plm.edu.ph>
2024-12-13 17:06:07 +0800

    fix admin

--- sampleapp/admin.py
+++ sampleapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Item
+from django.contrib.auth.admin import UserAdmin
+from .models import Item, CustomUser
+
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
@@ -10,3 +12,20 @@ class ItemAdmin(admin.ModelAdmin):
         if not change and Item.objects.filter(name__iexact=obj.name).exists():
             raise ValueError("An item with this name already exists.")
         super().save_model(request, obj, form, change)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'is_active', 'is_staff')}
+        ),
+    )
+    list_display = ('username', 'is_staff', 'is_active')
+    search_fields = ('username',)
+    ordering = ('username',)
